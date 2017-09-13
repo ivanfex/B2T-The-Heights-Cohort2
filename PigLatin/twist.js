df2b05ebcffff19f211c63d3c937287c678d6c5b
Ivan Felix <ivaneduardo68@gmail.com>
2017-09-13 10:52:24 -0400

    changed rest.toLocaleString to rest.toLowerCase

--- PigLatin/twist.js
+++ PigLatin/twist.js
@@ -16,7 +16,7 @@ if (word.charAt(0) == ("a") || word.charAt(0) == ("e") || word.charAt(0) == ("i"
     var first = word.charAt(0);
     var rest = word.substr(1,(word.length - 1));
     //Turning all letters to lower case that way the text is not going to look unkFay
-    var latin = rest.toLocaleLowerCase() + first.toLowerCase() + "ay";
+    var latin = rest.toLowerCase() + first.toLowerCase() + "ay";
 
     //console.log(rest.toLowerCase() + first.toLowerCase() + "ay"); //for testing purposes
     //console.log(latin); //for testing purposes
